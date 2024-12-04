d3afa82a5c26a3516ffdc233bd5da622c8f651f1
jacobwaxman11 <jacobwaxman21@gmail.com>
2024-12-03 22:46:50 -0500

    update

--- db.py
+++ db.py
@@ -13,11 +13,11 @@ NOT_FOUND = "NOT_FOUND"
 def get_market_verification(kalshi_ticker, polymarket_id):
     """Get the verification status of a market"""
     response = supabase.table('markets').select('*').eq('kalshi_ticker', kalshi_ticker).eq('polymarket_id', polymarket_id).execute()
-    
+    print('response', response )
     if response.data:
         return response.data[0]['is_match']
     return NOT_FOUND
-l
-def insert_market_verification(kalshi_ticker, polymarket_id, kalshi_title, , poly_question, is_match):
+
+def insert_market_verification(kalshi_ticker, polymarket_id, kalshi_title, poly_question, is_match):
     """Insert the verification status of a market"""
     supabase.table('markets').insert({'kalshi_ticker': kalshi_ticker, 'polymarket_id': polymarket_id, 'is_match': is_match, 'poly_question': poly_question, 'kalshi_title': kalshi_title}).execute()
